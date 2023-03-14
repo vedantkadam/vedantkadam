929cb0e647eafe62ae5c2a094884e672c0857840
vedantkadam <83024561+vedantkadam@users.noreply.github.com>
2023-03-14 16:01:39 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,8 +9,8 @@ def update_footer():
     footer = Path('../FOOTER.md').read_text()
     return footer.format(timestamp=timestamp)
 
-def update_readme_medium_posts(readme_base, join_on):
-    d = feedparser.parse()
+def update_readme_medium_posts(medium_feed, readme_base, join_on):
+    d = feedparser.parse(medium_feed)
     posts = []
     for item in d.entries:
         if item.get('tags'):
@@ -18,8 +18,8 @@ def update_readme_medium_posts(readme_base, join_on):
     posts_joined = '\n'.join(posts)
     return readme_base[:readme_base.find(rss_title)] + f"{join_on}\n{posts_joined}"
 
-rss_title = "" # Anchor for where to append posts
+rss_title = "### Stories by Dylan Roy on Medium" # Anchor for where to append posts
 readme = Path('../README.md').read_text()
-updated_readme = update_readme_medium_posts( readme, rss_title)
+updated_readme = update_readme_medium_posts("https://medium.com/feed/@vedantkadam541", readme, rss_title)
 with open('../README.md', "w+") as f:
     f.write(updated_readme + update_footer())
