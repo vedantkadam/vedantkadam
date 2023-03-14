53222efcf72ab0c73277a9d6ccf0adbbe4bb01d0
vedantkadam <83024561+vedantkadam@users.noreply.github.com>
2023-03-14 15:50:37 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,7 +9,17 @@ def update_footer():
     footer = Path('../FOOTER.md').read_text()
     return footer.format(timestamp=timestamp)
 
-readme = Path('../README.md').read_text()
+def update_readme_medium_posts(readme_base, join_on):
+    d = feedparser.parse()
+    posts = []
+    for item in d.entries:
+        if item.get('tags'):
+            posts.append(f" - [{item['title']}]({item['link']})")
+    posts_joined = '\n'.join(posts)
+    return readme_base[:readme_base.find(rss_title)] + f"{join_on}\n{posts_joined}"
 
+rss_title = "" # Anchor for where to append posts
+readme = Path('../README.md').read_text()
+updated_readme = update_readme_medium_posts( readme, rss_title)
 with open('../README.md', "w+") as f:
-    f.write(readme + update_footer())
+    f.write(updated_readme + update_footer())
